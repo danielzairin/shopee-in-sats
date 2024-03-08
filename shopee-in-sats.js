5dee63e933e4a0c20f745cbaa8c2b71ae2c81ea2
Daniel Zairin <danielzairink@gmail.com>
2024-03-08 21:28:46 +0800

    Refactor code, handle formatting

--- shopee-in-sats.js
+++ shopee-in-sats.js
@@ -1,39 +1,73 @@
 function main() {
+  /**@type {Date | null} */
+  let date = null;
+
+  // Search the 'order paid' stepper element for a date
   const orderPaidStepper = document.querySelector('[aria-label^="order paid"');
-  for (const node of orderPaidStepper.childNodes) {
-    if (!node.textContent) {
+  for (const elem of orderPaidStepper.childNodes) {
+    if (!elem.textContent) {
       continue;
     }
 
-    if (!isNaN(Date.parse(node.textContent))) {
-      console.log(`TODO: Get BTC price @ ${new Date(node.textContent)}`);
-      continue;
+    const parsedDate = Date.parse(elem.textContent);
+    if (!isNaN(parsedDate)) {
+      date = parsedDate;
+      break;
     }
-
-    const currencyRegex = /(-)?RM\d+(\.\d{2})?/g;
-    node.textContent = node.textContent.replaceAll(
-      currencyRegex,
-      "69,420 sats"
-    );
   }
 
-  const priceTagSelectors = [".Tfejtu", ".q6Gzj5", ".nW_6Oi"];
-  const priceTags = priceTagSelectors.flatMap((selector) => {
+  if (date === null) {
+    console.error("failed to find a date");
+    return;
+  }
+
+  const btcPrice = getBTCPriceOnDate(date);
+  const satsPerRinggit = Math.floor((1 / btcPrice) * 100_000_000);
+
+  // Select all 'line item' elements
+  const lineItemSelectors = [".Tfejtu", ".q6Gzj5", ".nW_6Oi"];
+  const lineItems = lineItemSelectors.flatMap((selector) => {
     const nodeList = document.querySelectorAll(selector);
     return Array.from(nodeList);
   });
 
-  for (const node of priceTags) {
-    if (!node.textContent) {
+  /**@type {Element[]} */
+  const elementsToConvert = new Array()
+    .concat(lineItems)
+    .concat(Array.from(orderPaidStepper.childNodes));
+
+  // Convert each element's text content to satoshis
+  for (const elem of elementsToConvert) {
+    if (!elem.textContent) {
       continue;
     }
-
-    const currencyRegex = /(-)?RM\d+(\.\d{2})?/g;
-    node.textContent = node.textContent.replaceAll(
-      currencyRegex,
-      "69,420 sats"
-    );
+    elem.textContent = toSats(elem.textContent, satsPerRinggit);
   }
+}
+
+/**
+ * Returns the price of 1 bitcoin in ringgit on a certain date.
+ * @param {Date} date
+ * @returns {number}
+ */
+function getBTCPriceOnDate(date) {
+  // TODO: Get actual price from somewhere
+  const price = date.valueOf() / 5_000_000;
+  return price;
+}
+
+/**
+ * Converts all ringgit price tags to satoshis.
+ * @param {string} str
+ * @param {number} satsPerRinggit
+ * @returns {string}
+ */
+function toSats(str, satsPerRinggit) {
+  const numberFormatter = new Intl.NumberFormat("en-US");
+  const regex = /(-)?RM(\d+(\.\d{2})?)/g;
+  return str.replaceAll(regex, (_match, _g1, price) => {
+    return `${numberFormatter.format(Math.floor(price * satsPerRinggit))} sats`;
+  });
 }
 
 main();
